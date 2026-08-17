597b4774ebcc14f1fcd0d4026217640467d8cda3
ave8tor <matt.krivohlavy@gmail.com>
2026-08-17 11:53:31 -0700

    Initial Commit
    MyE1000 Takeoff and Landing Data (TOLD) Calculator

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'told-calc-cache-v1';
+// Add all files that need to be saved for offline use
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(URLS_TO_CACHE);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return the cached version if found, otherwise fetch from the network
+        return response || fetch(event.request);
+      })
+  );
+});
